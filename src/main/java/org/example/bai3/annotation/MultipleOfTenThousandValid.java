1ec96055ed71e3c5dbb664920674be6c5ee79e0b
sonbui <thaisonb936@gmail.com>
2026-04-15 23:14:40 +0700

    fix null

--- src/main/java/org/example/bai3/annotation/MultipleOfTenThousandValid.java
+++ src/main/java/org/example/bai3/annotation/MultipleOfTenThousandValid.java
@@ -6,6 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 public class MultipleOfTenThousandValid implements ConstraintValidator<MultipleOfTenThousand, Double> {
     @Override
     public boolean isValid(Double value, ConstraintValidatorContext context) {
+        if (value == null) return false;
         if (value <= 0) return false;
         return value % 10000 == 0;
     }
